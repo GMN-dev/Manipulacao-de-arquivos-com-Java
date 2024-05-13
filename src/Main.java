5fc83f9ca91b3b28e2c771a18c488216bd143882
GMN-dev <g.magalhaes.dev@gmail.com>
2024-05-13 20:19:48 -0300

    FileReader e BufferedReader sem Try-with-resources

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,58 @@
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args)  {
 		
-		File file = new File("C:\\temp\\teste.txt");
-		Scanner sc = null;
-		try {
-			sc = new Scanner(file);
-			while(sc.hasNext()) {
-				System.out.println(sc.nextLine());
-			}	
-			sc.close();
-		}
-		catch(Exception err) {
-			System.out.println(err);
-		}finally {
-			if(sc != null) {
-				sc.close();
-			}
-		}
+//		File file = new File("C:\\temp\\teste.txt");
+//		Scanner sc = null;
+//		try {
+//			sc = new Scanner(file);
+//			while(sc.hasNext()) {
+//				System.out.println(sc.nextLine());
+//			}	
+//		}
+//		catch(Exception err) {
+//			System.out.println(err);
+//		}finally {
+//			if(sc != null) {
+//				sc.close();
+//			}
+//		}
 		
-	}
+		
+//		-=-=-=-=--=-=-=-=- FileReader/BufferedReader sem try-with-resources
+		
+//		String stringPath = "C:\\temp\\teste.txt";
+//		
+//		FileReader file = null;
+//		BufferedReader buff = null;
+//		
+//		try {
+//			file = new FileReader(stringPath);
+//			buff = new BufferedReader(file);
+//			
+//			String line = buff.readLine();
+//			
+//			while(line != null){
+//					System.out.println(line);
+//					line = buff.readLine();
+//			}
+//			
+//		}catch(Exception e) {
+//			System.out.println(e.getMessage());
+//		}finally {
+//			if(file != null && buff != null) {
+//				try {
+//					file.close();
+//					buff.close();
+//				}catch(Exception e) {
+//					System.out.println(e.getMessage());
+//				}
+//			}
+//		}
+		
+		
+	} 
 }
